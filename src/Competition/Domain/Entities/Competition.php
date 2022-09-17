4f4d4db9369dd102732d7406b22adcbdfb123176
Gerardo Herrera <ghgcoder@gmail.com>
2022-09-17 15:43:12 -0500

    add teams attribute to Competition entity and create team stub

--- src/Competition/Domain/Entities/Competition.php
+++ src/Competition/Domain/Entities/Competition.php
@@ -7,8 +7,17 @@ use BetPoolCore\Competition\Domain\VO\CompetitionName;
 class Competition{
     
 
+    /**
+     * constructor
+     * 
+     * TODO: array type pass to Colletion type for validate type object team
+     *
+     * @param CompetitionName $name
+     * @param array $teams
+     */
     public function __construct(
-        public CompetitionName $name
+        public CompetitionName $name,
+        public array $teams
     )
     {   
     }
@@ -17,20 +26,31 @@ class Competition{
     {   
         return $this->name;
     }
+    
+    public function getTeams():array
+    {   
+        return $this->teams;
+    }
 
     public static function create(
-        CompetitionName $name
+        CompetitionName $name,
+        array $teams
     ) :static
     {   
         return new static(
-            $name
+            $name,
+            $teams
         );
     }
 
-    public static function createFromPrimitives($name):static
+    public static function createFromPrimitives(
+        $name, 
+        array $teams
+        ):static
     {   
         return static::create(
-            new CompetitionName($name)
+            new CompetitionName($name),
+            $teams
         );
     }
 
--- tests/Competition/Domain/CompetitionTest.php
+++ tests/Competition/Domain/CompetitionTest.php
@@ -4,6 +4,7 @@ namespace BetPoolTest\Competition\Domain;
 
 use BetPoolCore\Competition\Domain\Entities\Competition;
 use BetPoolCore\Competition\Domain\VO\CompetitionName;
+use BetPoolTest\Team\Collaborators\TeamStub;
 use BetPoolTest\TestCase as BetPoolTestTestCase;
 
 class CompetitionTest extends BetPoolTestTestCase
@@ -12,21 +13,33 @@ class CompetitionTest extends BetPoolTestTestCase
     {
         $Name = new CompetitionName("FIFA WORLD CUP QATAR 2022");
 
+        $amountToCreateTEams = 12;
+
+        $Teams = TeamStub::createMany($amountToCreateTEams);
+
         $Competition = Competition::create(
             $Name,
+            $Teams
         );
 
         $this->assertInstanceOf(Competition::class, $Competition);
 
         $this->assertInstanceOf(CompetitionName::class, $Competition->getName());
+        
+        $this->assertTrue($amountToCreateTEams == count($Competition->getTeams()));
     }
 
     public function testCreateCompetitionEntitFromPrimitives()
     {
         $Name = "FIFA WORLD CUP QATAR 2022";
 
+        $amountToCreateTEams = 24;
+
+        $Teams = TeamStub::createMany($amountToCreateTEams);
+
         $Competition = Competition::createFromPrimitives(
             $Name,
+            $Teams,
         );
 
         $this->assertInstanceOf(Competition::class, $Competition);
