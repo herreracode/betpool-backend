a8a912d8edcdf84212ab8d05c9a08b0bae0f8930
root <root@DESKTOP-10UA9GV.localdomain>
2023-02-02 20:55:04 -0500

    laravel init with sail

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,18 +1,10 @@
 <?php
 
-namespace BetPoolTest;
+namespace Tests;
 
-use Laravel\Lumen\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    /**
-     * Creates the application.
-     *
-     * @return \Laravel\Lumen\Application
-     */
-    public function createApplication()
-    {
-        return require __DIR__.'/../bootstrap/app.php';
-    }
+    use CreatesApplication;
 }
