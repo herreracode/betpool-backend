f9f05de76184273614ba70a592afebe4d6eef92d
Gerardo Herrera <gerardo.herrera@funiber.org>
2023-02-10 18:21:09 -0500

    add prediction relations models

--- app/Models/Prediction.php
+++ app/Models/Prediction.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Prediction extends Model
 {
     use HasFactory;
+
+    /**
+     * get user
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * get Pools user
+     */
+    public function pool()
+    {
+        return $this->belongsTo(Pool::class);
+    }
 }
