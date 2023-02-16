07d51c949174bf3bcba4e17f4dc6b3e47db2fd80
Gerardo <ghgcoder@gmail.com>
2023-02-15 21:27:01 -0500

    create competition action

---
+++ app/Actions/Competition/CreateCompetition.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Actions\Competition;
+
+use App\Models\Competition;
+use Exception;
+
+/**
+ * Class CreateCompetition
+ * 
+ * @package App\Http\Actions\Competition
+ */
+class CreateCompetition
+{
+
+    public function __construct()
+    {
+        
+    }
+
+    public function __invoke($name)
+    {
+        $Competition = new Competition();
+
+        $Competition->name = $name;
+
+        if(!$Competition->save())
+            throw new Exception("dont save competition");
+
+        return $Competition;
+    }
+    
+}
