05c17a34419639825f56eae14bc95ae7d198b864
Gerardo <ghgcoder@gmail.com>
2023-03-18 16:30:41 -0500

    refactor to create pool action for better code readability

--- app/Actions/Pool/CreatePool.php
+++ app/Actions/Pool/CreatePool.php
@@ -4,34 +4,57 @@ namespace App\Actions\Pool;
 
 use App\Exceptions\Pool\CompetitionMustBeUniqueInAPool;
 use App\Models\Competition;
+use App\Models\Enums\RoleUsers;
 use App\Models\Pool;
 use App\Models\User;
 use Exception;
-use Illuminate\Support\Collection;
 use Spatie\Permission\Models\Role;
 
 /**
  * Class CreatePool
+ *
+ * @package App\Actions\Pool
  */
 class CreatePool
 {
 
     public function __invoke(
         User $UserCreator,
-        $namePool,
+        string $namePool,
         iterable $competitions = null
     ): Pool {
 
+        $this->haveCompetitionUnique($competitions);
+
+        $Pool = $this->createPool($namePool);
+
+        $this->assingRoleUserCreator($UserCreator, $Pool);
+
+        //add user creator
+        $Pool->users()->attach($UserCreator);
+
+        //add competitions to pool
+        $competitions && $Pool->competitions()->attach($competitions);
+
+        return $Pool;
+    }
+
+    protected function haveCompetitionUnique(iterable $competitions = null)
+    {
         $competitionsCollect = collect($competitions);
 
-        $haveCompetitionUnique = $competitionsCollect->count() > 1 && $competitionsCollect->filter(function (Competition $competition){
-            return $competition->must_be_unique;
-        })->count() > 0;
+        $haveMoreThanOne = $competitionsCollect->count() > 1;
 
-        if($haveCompetitionUnique){
+        $haveUniqueCompetition = $competitionsCollect->filter(function (Competition $competition){
+                return $competition->must_be_unique;
+            })->count() > 0;
+
+        if($haveMoreThanOne && $haveUniqueCompetition)
             throw new CompetitionMustBeUniqueInAPool('Have competition unique');
-        }
+    }
 
+    protected function createPool(string $namePool)
+    {
         $Pool = new Pool();
 
         $Pool->name = $namePool;
@@ -40,20 +63,17 @@ class CreatePool
             throw new Exception('dont save Pool');
         }
 
+        return $Pool;
+    }
+
+    protected function assingRoleUserCreator(User $UserCreator, Pool $Pool)
+    {
         setPermissionsTeamId($Pool->id);
 
         $RolePoolAdmin = Role::create([
-            'name' => '_POOL_ADMIN_'
+            'name' => RoleUsers::PoolAdmin
         ]);
 
         $UserCreator->assignRole($RolePoolAdmin);
-
-        //add users
-        $Pool->users()->attach($UserCreator);
-
-        //add competitions to pool
-        $competitions && $Pool->competitions()->attach($competitions);
-
-        return $Pool;
     }
 }
