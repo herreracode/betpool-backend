4bab04f2aa174303ffb7bd942b098c6b43785c9c
Gerardo <ghgcoder@gmail.com>
2023-08-09 21:20:08 -0500

    feature: create pool with action and front-end integration

--- app/Http/Controllers/ApiControllers/PoolPostController.php
+++ app/Http/Controllers/ApiControllers/PoolPostController.php
@@ -2,18 +2,37 @@
 
 namespace App\Http\Controllers\ApiControllers;
 
+use App\Actions\Pool\CreatePool;
 use App\Http\Controllers\Controller;
+use App\Models\Competition;
 use Illuminate\Http\Request;
 
 class PoolPostController extends Controller
 {
-    public function __invoke(Request $request){
+    public function __construct(public CreatePool $createPool)
+    {
 
-       return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA',
-        ]);
 
     }
-    
+
+    public function __invoke(Request $request)
+    {
+        $idsCompetition = $request->get('competitions');
+
+        $Competitions = Competition::whereIn('id', $idsCompetition)->get();
+
+        $Pool = $this->createPool->__invoke(
+            auth()->user(),
+            $request->get('name'),
+            $Competitions
+        );
+
+
+        return response()->json([
+            'status' => 'true',
+            'item' => $Pool->only(['id', 'name'])
+        ], 201);
+
+    }
+
 }
