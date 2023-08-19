6e428968547fbfd55b7b321dcdc22fd7a54467c8
Gerardo <ghgcoder@gmail.com>
2023-08-19 13:40:45 -0500

    feature: action to close predictions by pool round

--- app/Models/PoolRound.php
+++ app/Models/PoolRound.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\DomainServices\Prediction\CalculateNumberOfPointsEarned;
+use App\Models\Enums\PoolRoundStatus;
+use App\Models\Enums\PredictionStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -25,5 +28,30 @@ class PoolRound extends Model
     public function predictions()
     {
         return $this->hasMany(Prediction::class);
+    }
+
+    public function closePredictions(Game $Game, CalculateNumberOfPointsEarned $calculateNumberOfPointsEarned) :PoolRound
+    {
+        $predictionsByGame = $this->predictions
+        ->filter(fn (Prediction $Prediction) => $Prediction->game->id == $Game->id);
+
+        $predictionsByGame->each(fn(Prediction $Prediction) => $Prediction->close($calculateNumberOfPointsEarned));
+
+        $predictionsStatus = $this->predictions
+        ->pluck('status')
+        ->unique();
+
+        //validate all predictions state in finish
+        if($predictionsStatus->count() == 1 &&  $predictionsStatus->first() == PredictionStatus::CLOSE->value)
+            $this->finish();
+
+        return $this;
+    }
+
+    protected function finish()
+    {
+        $this->status = PoolRoundStatus::FINISH->value;
+
+        $this->save();
     }
 }
