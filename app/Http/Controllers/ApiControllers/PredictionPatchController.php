a2b68ba2236da2e4c168e98311638b575153e46a
Gerardo <ghgcoder@gmail.com>
2023-08-21 20:41:57 -0500

    feature: prediction patch integration

--- app/Http/Controllers/ApiControllers/PredictionPatchController.php
+++ app/Http/Controllers/ApiControllers/PredictionPatchController.php
@@ -4,31 +4,44 @@ namespace App\Http\Controllers\ApiControllers;
 
 use App\Actions\PoolRound\CreatePoolRound;
 use App\Actions\Prediction\CreatePrediction;
+use App\Actions\Prediction\ModifyPrediction;
 use App\Http\Controllers\Controller;
 use App\Models\Competition;
 use App\Models\Game;
 use App\Models\Pool;
 use App\Models\PoolRound;
+use App\Models\Prediction;
 use Illuminate\Http\Request;
 
 class PredictionPatchController extends Controller
 {
 
-    public function __construct(public CreatePrediction $createPrediction)
+    public function __construct(public ModifyPrediction $modifyPrediction)
     {
     }
 
 
     public function __invoke($idPrediction, Request $request)
     {
-        //$request->get('prediction');
+        $dataPrediction = $request->get('prediction');
+
+        $Prediction = Prediction::find($idPrediction);
 
         //todo: services to edit predictions
-       
+
+        $this
+            ->modifyPrediction
+            ->__invoke(
+                $Prediction,
+                $dataPrediction['score_local'],
+                $dataPrediction['score_away'],
+                auth()->user()->id
+            );
+
         return response()->json([
             'status' => 'true',
             'item' => "hola"
-        ], 201);
+        ], 200);
     }
 
 }
