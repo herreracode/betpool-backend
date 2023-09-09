e9cb630e4a1e6bfbcab2a0f24a2190714e307cd2
Gerardo <ghgcoder@gmail.com>
2023-09-09 14:56:29 -0500

    feature: initial seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Artisan;
 
 class DatabaseSeeder extends Seeder
 {
@@ -24,7 +25,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $this->call(CompetitionSeeder::class);
-        $this->call(TeamSeeder::class);
-        $this->call(GameSeeder::class);
+
+        Artisan::call('games:create-games-by-external-api');
     }
 }
