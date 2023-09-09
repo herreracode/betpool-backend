f67513acca249bf80648c9e947e9cbf75e430443
Gerardo <ghgcoder@gmail.com>
2023-09-09 17:32:24 -0500

    feature: handler backend for excepctions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,49 +2,165 @@
 
 namespace App\Exceptions;
 
+use App\Exceptions\PoolRound\AlreadyHavePoolRoundPending;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Response;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
+     * A list of the exception types that should not be reported.
      *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
-     * A list of the exception types that are not reported.
-     *
-     * @var array<int, class-string<\Throwable>>
+     * @var array
      */
     protected $dontReport = [
-        //
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        ValidationException::class,
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the NOT FOUND exceptions.
      *
-     * @var array<int, string>
+     * @var array
      */
-    protected $dontFlash = [
-        'current_password',
-        'password',
-        'password_confirmation',
+    protected $notFoundExceptions = [
     ];
 
     /**
-     * Register the exception handling callbacks for the application.
+     * A list of the BAD REQUEST exceptions.
      *
+     * @var array
+     */
+    protected $badRequestExceptions = [
+        AlreadyHavePoolRoundPending::class
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $unauthorizedExceptions = [
+    ];
+
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Throwable  $exception
      * @return void
+     *
+     * @throws \Exception
      */
-    public function register()
+    public function report(Throwable $exception)
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+        parent::report($exception);
+    }
+
+    /**
+     * Render an exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Throwable  $exception
+     * @return \Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Throwable
+     */
+    public function render($request, Throwable $exception)
+    {
+        if (env('APP_DEBUG'))
+            return parent::render($request, $exception);
+
+        $status = Response::HTTP_INTERNAL_SERVER_ERROR;
+
+        if ($exception instanceof MethodNotAllowedHttpException)
+        {
+            $status = Response::HTTP_METHOD_NOT_ALLOWED;
+            $exception = new MethodNotAllowedHttpException([], 'HTTP_METHOD_NOT_ALLOWED', $exception);
+        }
+        else if ($this->isNotFoundException($exception))
+        {
+            $status = Response::HTTP_NOT_FOUND;
+            $exception = new NotFoundHttpException($exception->getMessage() ?? 'HTTP_NOT_FOUND', $exception);
+        }
+        else if ($this->isBadRequestException($exception))
+        {
+            $status = Response::HTTP_BAD_REQUEST;
+            $exception = new HttpException($status, $exception->getMessage() ?? 'HTTP_BAD_REQUEST', $exception);
+        }
+        else if ($this->isUnauthorizedException($exception))
+        {
+            $status = Response::HTTP_UNAUTHORIZED;
+            $exception = new HttpException($status, $exception->getMessage() ?? 'HTTP_UNAUTHORIZED', $exception);
+        }
+        else if ($exception instanceof ValidationException)
+        {
+            $status = Response::HTTP_UNPROCESSABLE_ENTITY;
+            $exception = new HttpException($status, $exception->getResponse()->getContent());
+        }
+        else if ($exception)
+        {
+            $exception = new HttpException($status, $exception->getMessage());
+        }
+
+        $message = $exception->getMessage();
+
+        return response()->json([
+            'error' => [
+                'message' => json_decode($message) ?? $message,
+                'status' => $status,
+                'id' => class_basename($exception->getPrevious()),
+            ],
+        ], $status);
+    }
+
+
+    /**
+     * @param Throwable $exception
+     * @return bool
+     */
+    private function isNotFoundException(Throwable $exception) : bool
+    {
+        return collect($this->notFoundExceptions)
+            ->contains($this->validateInstanceOf($exception));
+    }
+
+    /**
+     * @param Throwable $exception
+     * @return bool
+     */
+    private function isBadRequestException(Throwable $exception): bool
+    {
+        return collect($this->badRequestExceptions)
+            ->contains($this->validateInstanceOf($exception));
+    }
+
+    /**
+     * @param Throwable $exception
+     * @return bool
+     */
+    private function isUnauthorizedException(Throwable $exception): bool
+    {
+        return collect($this->unauthorizedExceptions)
+            ->contains($this->validateInstanceOf($exception));
+    }
+
+    /**
+     * @param Throwable $exception
+     * @return \Closure
+     */
+    protected function validateInstanceOf(Throwable $exception): \Closure
+    {
+        return function ($exceptionClass) use ($exception) {
+            return $exception instanceof $exceptionClass;
+        };
     }
 }
