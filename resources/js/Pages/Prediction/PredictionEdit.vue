bfd26aaec6a954ebb090f9c26bfcfffbf8adefce
Gerardo <ghgcoder@gmail.com>
2023-09-16 14:19:13 -0500

    feature: prediction edit mandatory fields

--- resources/js/Pages/Prediction/PredictionEdit.vue
+++ resources/js/Pages/Prediction/PredictionEdit.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import {ref} from 'vue';
 import Prediction from "@/Models/Prediction";
-import { Link, router } from "@inertiajs/vue3"
+import {Link, router} from "@inertiajs/vue3"
 import HttpClient from '@/Shared/HttpClient';
+import {isMandatoryField, formValidate} from '@/Shared/validateForms';
 import {useToast} from 'vue-toast-notification';
+
 const $toast = useToast();
+
+const formEditPrediction = ref(null)
 
 interface Props {
     prediction: Prediction,
@@ -12,6 +16,11 @@ interface Props {
 }
 
 const editPredictions = async () => {
+
+    let validForm: boolean = await formValidate(formEditPrediction)
+
+    if (!validForm)
+        return
 
     try {
 
@@ -35,6 +44,10 @@ const props = defineProps<Props>()
 const score_local = ref(null)
 const score_away = ref(null)
 
+const ruleFieldScorePredictions = [
+    isMandatoryField
+];
+
 </script>
 
 <template>
@@ -48,33 +61,37 @@ const score_away = ref(null)
         </v-toolbar-items>
     </v-toolbar>
     <v-card>
-        <v-card>
-            <v-card-text>
-                <v-container>
-                    <v-row>
-                        <v-col cols="4">
-                            <span>{{ prediction.team_local }}</span> <v-text-field v-model="prediction.score_local"
-                                hide-details single-line type="number" />
-                        </v-col>
-                        <v-col cols="4">
-                            <span>{{ prediction.team_away }}</span> <v-text-field v-model="prediction.score_away"
-                                hide-details single-line type="number" />
-                        </v-col>
-                    </v-row>
-                </v-container>
-                <small>*indicates required field</small>
-            </v-card-text>
-            <v-card-actions>
-                <v-spacer></v-spacer>
-                <Link :href="route('pool-round.indiviual-view', pool_round_id)"
-                    class="v-btn v-btn--elevated v-theme--light bg-error v-btn--density-default v-btn--size-default v-btn--variant-elevated">
-                Cancelar
-                </Link>
+        <v-form @submit.prevent ref="formEditPrediction">
+            <v-card>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="4">
+                                <span>{{ prediction.team_local }}</span>
+                                <v-text-field v-model="prediction.score_local"
+                                              single-line type="number" :rules="ruleFieldScorePredictions"/>
+                            </v-col>
+                            <v-col cols="4">
+                                <span>{{ prediction.team_away }}</span>
+                                <v-text-field v-model="prediction.score_away"
+                                              single-line type="number" :rules="ruleFieldScorePredictions"/>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <Link :href="route('pool-round.indiviual-view', pool_round_id)"
+                          class="v-btn v-btn--elevated v-theme--light bg-error v-btn--density-default v-btn--size-default v-btn--variant-elevated">
+                        Cancelar
+                    </Link>
 
-                <v-btn color="blue-darken-1" variant="text" @click="editPredictions()">
-                    Guardar
-                </v-btn>
-            </v-card-actions>
-        </v-card>
+                    <v-btn color="blue-darken-1" type="submit" variant="text" @click="editPredictions()">
+                        Guardar
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-form>
     </v-card>
 </template>
